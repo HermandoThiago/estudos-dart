3912151fc454e291c175a1fbac721f2a05e8d6ea
Hermando Thiago <hermando2013@gmail.com>
2022-04-07 09:19:51 -0300

    resolved map exercise

--- map.dart
+++ map.dart
@@ -1,10 +1,12 @@
 void main(){
 
   final Map<String, String> map = {
-  "1": "first_item",
-  "2": "second_item",
-  "3": "third_item"
-};
+
+    "1": "first_item",
+    "2": "second_item",
+    "3": "third_item"
+
+  };
 
   // Printar o map
   print(map);
@@ -40,18 +42,19 @@ void main(){
   }
 
   // percorra cada par chave-valor do map e printe na tela seus respectivos valores de chave e valor.
-  map.forEach((key, value) {print("$key $value");});
+  map.forEach((key, value) { print("$key $value"); });
 
   // remova a chave "3" do map e printe o map atualizado.
   map.remove("3");
   print(map);
 
-  // remova todas as chaves cujo o valor seja uma string que possua mais do que 4 caracteres.
-  
-
   // crie um map a partir do map já existente. O novo map deverá ter as mesmas chaves,
   // mas o valor das chaves deverá ser igual ao comprimentos das Strings dos valores do primeiro map
   final Map<String, String> map2 = map.map((String key, String value) => MapEntry(key, value.length.toString()));
   print(map2);
+
+  // remova todas as chaves cujo o valor seja uma string que possua mais do que 4 caracteres.
+  map.removeWhere((key, value) => value.length > 4);
+  print(map);
 
 }
